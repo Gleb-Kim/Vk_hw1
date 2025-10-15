f18ca955a51c01f18793a117d84d5df2886346cc
Gleb-Kim <63071738+Gleb-Kim@users.noreply.github.com>
2025-10-16 00:36:09 +0300

    Update reversePartOfArray.py

--- reversePartOfArray.py
+++ reversePartOfArray.py
@@ -6,14 +6,22 @@ def reverseArrayLR(arr, left, right):
 
 def reversePartOfArray(arr, k):
     n = len(arr)
+    if n == 0:
+        return
+    rem = k % n
+    if rem == 0:
+        return
     reverseArrayLR(arr, 0, n - 1)
-    reverseArrayLR(arr, 0, (k - 1) % n)
-    reverseArrayLR(arr, k % n, n - 1)
+    reverseArrayLR(arr, 0, rem - 1)
+    reverseArrayLR(arr, rem, n - 1)
 
 
 mas = [1, 2, 3, 4, 5, 6, 7]
 print(mas)
+reversePartOfArray(mas, 7)
+print(mas)
 reversePartOfArray(mas, 3)
 print(mas)
-reversePartOfArray(mas, 11)
+mas = []
+reversePartOfArray(mas, 3)
 print(mas)
